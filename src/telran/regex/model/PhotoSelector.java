3923ede94fc636acadb2d5f6a8d29e355fcffb6b
Denis <Dehimmaur@gmail.com>
2025-06-16 19:58:13 +0300

    v1_7_2 Tests: allEurope allAutumn all 2015

--- src/telran/regex/model/PhotoSelector.java
+++ src/telran/regex/model/PhotoSelector.java
@@ -1,13 +1,16 @@
 package telran.regex.model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PhotoSelector {
-
-
-    public static String[] selectPictures(String pictures, String regex){
+    public static String selectPictures(String pictures, String regex){
         StringBuilder strRes = new StringBuilder();
-        
-
-
-
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(pictures);
+        while (matcher.find()) {
+                strRes.append("\"").append(matcher.group()).append("\"").append(",\n");
+        }
+        return strRes.toString().length() > 2 ? strRes.toString().substring(0, strRes.toString().length() - 2) : "";
     }
 }
--- src/telran/regex/tests/PhotoSelectorTest.java
+++ src/telran/regex/tests/PhotoSelectorTest.java
@@ -2,6 +2,8 @@ package telran.regex.tests;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static telran.regex.model.PhotoSelector.selectPictures;
 
 public class PhotoSelectorTest {
@@ -19,7 +21,7 @@ public class PhotoSelectorTest {
             "London\\20151001_121705.jpg",				
             "London\\20151001_231035.jpg",
             "Chicago\\20150301_120001.png",
-            "Chicago\\20151111_232000.png"
+            "Chicago\\20151111_232000.png
             """;
 
 
@@ -29,14 +31,70 @@ public class PhotoSelectorTest {
 
     @Test
     void testAllEuropePictures(){
-        String regex = "^(Paris|London).*";
-        String[] actual = selectPictures(FILE_DATA, regex);
+        String regex = "(Paris|London)\\\\[^\\s,\"]+";
+        String actual = selectPictures(FILE_DATA, regex);
+        String expected = """
+                "Paris\\20140101_090000.jpg",
+                "Paris\\20140201_121005.jpg",
+                "Paris\\20150301_211035.jpg",
+                "Paris\\20150401_110023.gif",
+                "Paris\\20150401_181705.jpg",
+                "Paris\\20150501_231035.gif",
+                "London\\20140205_090000.jpg",
+                "London\\20140205_121005.jpg",
+                "London\\20140601_211035.gif",
+                "London\\20151001_110023.jpg",
+                "London\\20151001_121705.jpg",
+                "London\\20151001_231035.jpg\"""";
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    void testAutumnPictures() {
+        String regex = "([A-Za-z]+\\\\\\d{4}(09|10|11)\\d{2}_\\d{6}\\.(jpg|png|gif))";
+        String actual = selectPictures(FILE_DATA, regex);
+        String expected = """
+                "London\\20151001_110023.jpg",
+                "London\\20151001_121705.jpg",
+                "London\\20151001_231035.jpg",
+                "Chicago\\20151111_232000.png\"""";
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    void testPhotosByYear() {
+        String regex = "([A-Za-z]+\\\\2015\\d{4}_\\d{6}\\.(jpg|png|gif))";
+        String actual = selectPictures(FILE_DATA, regex);
+        String expected = """
+                "Paris\\20150301_211035.jpg",
+                "Paris\\20150401_110023.gif",
+                "Paris\\20150401_181705.jpg",
+                "Paris\\20150501_231035.gif",
+                "London\\20151001_110023.jpg",
+                "London\\20151001_121705.jpg",
+                "London\\20151001_231035.jpg",
+                "Chicago\\20150301_120001.png",
+                "Chicago\\20151111_232000.png\"""";
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    void testNightPictures() {
+        String regex = "([A-Za-z]+\\\\\\d{8}_(1[89]|2[0-3])\\d{4}\\.(jpg|png|gif))";
+        String actual = selectPictures(FILE_DATA, regex);
+        String expected = """
+                "Paris\\20150301_211035.jpg",
+                "Paris\\20150401_181705.jpg",
+                "Paris\\20150501_231035.gif",
+                "London\\20140601_211035.gif",
+                "London\\20151001_231035.jpg",
+                "Chicago\\20151111_232000.png\"""";
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    void testNightChicagoPictures(){
+
+        
     }
 }
-/*
-
- */
